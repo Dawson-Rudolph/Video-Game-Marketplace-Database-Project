3986557119288d060a4dcb4289627a44f4629807
Dawson Rudolph <dawsonrudolph03@gmail.com>
2025-05-04 13:19:07 -0500

    Adding library imports, addition documentation link, & functions file structure

--- databaseDriverLibrariesInformation.py
+++ databaseDriverLibrariesInformation.py
@@ -1,7 +1,7 @@
 # MongoDB (pymongo) information: https://www.mongodb.com/resources/languages/python
 # pip install pymongo
 
-# PostgreSQL (psycopg2) information: https://pypi.org/project/psycopg2/
+# PostgreSQL (psycopg2) information: https://pypi.org/project/psycopg2/ & https://www.geeksforgeeks.org/introduction-to-psycopg2-module-in-python/
 # pip install psycopg2-binary
 
 # Neo4j (neo4j) information: https://neo4j.com/docs/python-manual/current/
--- databaseQueryFunctions.py
+++ databaseQueryFunctions.py
@@ -1,1 +1,16 @@
 # This file will contain the 6 total queries for the 3 databases (2 queries for each DB)
+
+# Imports
+from pymongo import MongoClient
+import psycopg2
+from neo4j import GraphDatabase
+
+# MongoDB queries
+
+
+
+# PostreSQL queries
+
+
+
+# Neo4j queiries
